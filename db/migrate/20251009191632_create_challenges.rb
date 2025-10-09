544d7d9a6abde4df40eb1521d1dc209199f197df
S-Letelier <siletelier@miuandes.cl.com>
2025-10-09 16:38:23 -0300

    tabla challenge, migracion en progreso

--- db/migrate/20251009191632_create_challenges.rb
+++ db/migrate/20251009191632_create_challenges.rb
@@ -1,6 +1,13 @@
 class CreateChallenges < ActiveRecord::Migration[8.0]
   def change
     create_table :challenges do |t|
+      t.string :name
+      t.text :description
+      t.datetime :start_date
+      t.datetime :end_date
+      t.string :visibility
+      t.string :status
+
       t.timestamps
     end
   end
