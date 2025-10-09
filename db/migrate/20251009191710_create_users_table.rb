2b613e43a2a64b2b7a808d0d83551af5725d803d
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-09 16:27:56 -0300

    feat: Create migration for user model

--- db/migrate/20251009191710_create_users_table.rb
+++ db/migrate/20251009191710_create_users_table.rb
@@ -1,6 +1,12 @@
 class CreateUsersTable < ActiveRecord::Migration[8.0]
   def change
     create_table :users_tables do |t|
+      t.string :username
+      t.string :role
+      t.string :email
+      t.string :password
+      t.text :avatar_url
+
       t.timestamps
     end
   end
