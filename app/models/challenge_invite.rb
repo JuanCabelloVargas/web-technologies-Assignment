2fe12a36305e01986cadee8bedfbbf442a20bd0b
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-10 18:19:27 -0300

    fix: fix typo in class_name

--- app/models/challenge_invite.rb
+++ app/models/challenge_invite.rb
@@ -1,7 +1,7 @@
 class ChallengeInvite < ApplicationRecord
     belong_to :challenge
-    belong_to :inviter_id, class name: "users"
-    belong_to :invitee_user_id, class name: "users"
+    belong_to :inviter_id, class_name: "users"
+    belong_to :invitee_user_id, class_name: "users"
     validates :inviter_id, :invitee_user_id, uniqueness: true, presence: true
     validates :accepted_at, :is_accepted, presence: true
 end
