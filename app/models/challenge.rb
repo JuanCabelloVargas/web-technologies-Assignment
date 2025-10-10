274ddc2295080e0936723fb541d5345ed4ff99c1
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-10 18:18:37 -0300

    feat: Add associations to challenge model

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,16 @@
 class Challenge < ApplicationRecord
-    belong_to :user
-    belong_to :categorie
+    belongs_to :creator, class_name: "User"
+    belongs_to :category
+
+    has_many :challenge_requests, dependent: :destroy
+    has_many :challenge_comments, dependent: :destroy
+    has_many :challenge_participations, dependent: :destroy
+    has_many :challenge_invites, dependent: :destroy
+    has_many :bookmarked_challenges, dependent: :destroy
+    has_many :user_badges, dependent: :nullify
+    has_many :progress_logs, dependent: :destroy
+
+    has_many :bookmarkers, through: :bookmarked_challenges, source: :user
 
     validates :name, :description, :start_date, :end_date, :visibility, :status, presence: true
-    
-
 end
