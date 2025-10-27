3036674a3cf3a22b24703eab40a0410a6bb92343
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-27 19:33:40 -0300

    feat: Add cancancan authorization to users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,20 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    if user.role == "admin"
+    if user.admin?
       can :manage, :all
     else
-      can :read, :all
+      can :read, [ Category, Challenge, ChallengeParticipation, ProgressLog, Badge, UserBadge, ChallengeComment, Notification ]
+
+      if user.persisted?
+        can :read, User, id: user.id
+        can :update, User, id: user.id
+
+        can :create, [ ChallengeRequest, ChallengeInvite, ChallengeComment, BookmarkedChallenge, ProgressLog ]
+        can [ :update, :destroy ], Challengecomment, user_id: user.id
+        can [ :index, :create, :destroy, :toggle ], BookmarkedChallenge, user_id: user.id
+        can :mark_as_read, Notification, user_id: user.id
+      end
     end
   end
 end
