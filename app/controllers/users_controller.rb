65149874b2c0ef8cc7ad521e6827c37c888fd4eb
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-26 23:29:16 -0300

    feat: Add crud to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [ :show ]
+  load_and_authorize_resource
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+
 
   def index
     @users = User.all
@@ -8,9 +10,40 @@ class UsersController < ApplicationController
   def show
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "User Created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "User Updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: "User eliminated"
+  end
+
+
+
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :role, :avatar_url, :password)
   end
 end
