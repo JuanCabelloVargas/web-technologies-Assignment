07c8d935e13d754435da45a9c8e8f98bca7b9538
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-26 23:50:45 -0300

    feat: Create crud for badges

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,5 +1,8 @@
 class BadgesController < ApplicationController
-  before_action :set_badge, only: [ :show ]
+  load_and_authorize_resource
+  before_action :set_badge, only: [ :show, :edit, :update, :destroy ]
+
+
   def index
     @badges = Badge.all
   end
@@ -7,9 +10,42 @@ class BadgesController < ApplicationController
   def show
   end
 
+  def new
+    @badge = Badge.new
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+    if @badge.save
+      redirect_to @badge, notice: "Badge created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to @badge, notice: "Badge updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to badges_path, notice: "Badge deleted"
+  end
+
   private
 
   def set_badge
     @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:code, :name, :description, :icon_url)
   end
 end
