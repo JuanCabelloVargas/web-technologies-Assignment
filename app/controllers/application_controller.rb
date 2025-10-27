f0fe39bb8029438fe7cc84cf7149f3c71ff388f6
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-26 23:33:48 -0300

    feat: Add cancancan access denied exception method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  
+
   def current_user
     User.find(18)  # reemplaza 18 con un ID válido de tu base de datos
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
+
   helper_method :current_user
 end
