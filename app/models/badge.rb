ce153a2004ab1237f40c6e41023588d9c223b2a3
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-28 09:43:35 -0300

    feat: update badge model with asociations and validations

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,17 @@
 class Badge < ApplicationRecord
-    validates :name, :description, :icon_url, presence: true
-    validates :code, uniqueness: true, presence: true
+  has_many :user_badges, dependent: :destroy
+  has_many :users, through: :user_badges
+
+  validates :name, :description, :icon_url, presence: true
+  validates :code, uniqueness: true, presence: true
+  validates :description, presence: true
+
+
+  def awarded_to?(user)
+    users.exists?(user.id)
+  end
+
+  def awarded_count
+    user_badges.count
+  end
 end
