c4b97928ef4c85f4b996839bffb2d1b9a078a4c2
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-28 09:58:12 -0300

    feat: add association, validations and normalize name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,15 @@
 class Category < ApplicationRecord
-    validates :name, presence: true
+  has_many :challenges, dependent: :nullify
+
+  before_validations :normalize_name
+
+
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 80 }
+
+  private
+
+  def normalize_name
+    self.name = name.to_s.strip.squeeze(" ")
+  end
 end
